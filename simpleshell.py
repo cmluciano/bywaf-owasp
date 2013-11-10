f35b59904b3115212c72ce2a035b1005b06fc392
Roey Katz <roey.katz@gmail.com>
2013-11-10 15:15:48 -0500

    Module for opening, closing and selecting among local and remote (SSH) shells

---
+++ simpleshell.py
@@ -0,0 +1,151 @@
+# exampleplugin.py: a model plugin implementing an array of simple shells for
+# local and remote POSIX systems.  Users can open, close and select among local 
+# and remote SSH shells.
+
+print "loaded simple shell"
+
+# required dictionary
+# read when creating new shells
+options = {
+  # <name>    <value>    <default value>   <required>  <description>
+  ('RHOST',      '',            '',         'yes',     'IP address of the remote host'),
+  ('PORT',       '',            '22/tcp',   'no',      'Port/protocol of the remote host'),
+  ('CWD',        '',            '',         'no',      'Current working directory to set on the remote host account'),
+  ('USERNAME',   '',            '',         'yes',     'User name of remote host account'),
+  ('PASSWORD',   '',            '',         'yes',     'User password of remote host account')
+  }
+
+  
+# abstract shell-like class
+class SimpleShell:
+    
+    # open a new shell-like object, with current_working_directory defaulting to None
+    def __init__(self, current_working_directory=None):
+        self.cwd = current_working_directory
+    
+    def setcwd(self, newcwd):
+        self.cwd = newcwd
+    
+    def getcwd(self):
+        return self.cwd
+    
+    def send(self, text):
+        pass
+    
+    def receive(self):
+        pass
+
+# FIXME: implement this    
+class SimpleSSHShell(SimpleShell):
+
+    def __init__(self, current_working_directory, remote_host, remote_port, username, password):
+        super(SimpleSSHShell).__init__(current_working_directory)
+        
+        self.rhost = remote_host
+        self.rport = remote_port.split('/')[0]
+        self.protocol = remote_port.split('/')[1]
+        self.username = username
+        self.pw = password
+        
+        # fixme: implement the rest
+        
+
+# FIXME:  implement this        
+class SimpleLocalShell(SimpleShell):
+    pass
+
+
+# a list of SimpleShell instances
+shell_sessions = []
+
+# a reference to an element in shell_sessions
+current_shell_session = None
+    
+# utility function to set the prompt to reflect the current working directory  
+def set_prompt():
+    import os
+    cwd = os.getcwd()
+    app.set_prompt('exampleshell: {}'.format(cwd))
+    
+def shell_input_handler(line):
+    """simulate the system shell"""
+    
+    # insert code here to run a command through the open shell object, assumes
+    try:
+        current_shell_session.run(line)
+    except TypeError: # shellobject==None
+        print("no shells currently open")
+        return
+        
+    print(output)    
+    set_prompt()
+
+    
+def do_cd(line):
+    """change the current working directory"""
+    
+    newcwd = line.split(0)
+    
+    # insert code here to make sure that this is a valid directory for the shell object
+    
+    try:
+        current_shell_session.setcwd(newcwd)
+    except TypeError: # shellobject==None
+        print("no shells currently open")
+    except IOError: # target dir not found
+        print("target directory not found")
+
+        
+# FIXME:  Implement this
+# note, this is a multi-level command
+def do_shell(line):
+    """open, select and close multiple local and remote shells"""
+    
+    # subcommands to expose to the user:
+        
+    # new
+    #   local   
+    #   remote  - takes optional remote host, remote port, username, 
+    #             password (also settable through options{})
+    # select    - takes shell title or number to switch focus to
+    #              calls app.set_delegate_input_handler(shell_input_handler)
+    #              calls set_prompt()
+    # close     - takes shell title or number to switch focus to
+    
+
+# FIXME:  Implement this
+def complete_shell(self,text,line,begin_idx,end_idx):
+    pass
+
+def do_back_to_bywaf(self):
+    """cancel input delegation"""
+    app.unset_delegate_input_handler()
+    app.set_prompt(app.current_plugin_name)
+    
+    
+def do_bw(line):
+    """executes a command in bywalf"""
+    # note: get completion to work for this.  Tried overriding 
+    # Cmd.complete() to get past its reliance on
+    # readline.get_line_buffer(), but things got hairy (and
+    # overriding complete() is too much anyway)
+       
+    # Inject the rest of the command into the Bywaf command queue.    
+    # what I really want is to have working tab completion for this.
+    # note, this won't happen since delegate gets called regardless...fix this
+    app.cmdqueue.append(line[3:]) # skip past the 'bw '
+
+    
+# FIXME:  Implement this    
+def complete_shell(self,text,line,begin_idx,end_idx):
+    pass
+
+
+
+
+# FIXME:  Implement this
+# code is borrowed from Cmd.complete(), and makes necessary changes to give priority to 
+# commands in this module (otherwise they are skipped because the delegate input function gets executed)
+def complete(
+    
+       
