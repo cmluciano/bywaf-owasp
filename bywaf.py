538d6e29947673cb3c9bc1afd0387f9c50579e72
Noobay <Noobay@users.noreply.github.com>
2013-11-17 17:51:14 +0200

    Update bywaf.py
    changed exception handling to be Python3 compliant

--- bywaf.py
+++ bywaf.py
@@ -854,7 +854,7 @@ class WAFterpreter(Cmd):
 def interpreter_loop():
     try:
         wafterpreter.cmdloop()
-    except Exception, e:
+    except Exception as e:
         print '\nerror encountered, continue[Any-Key], show stack trace and continue[SC], show stack trace and quit[S]'
         answer = raw_input()
         if answer == 'S' or answer == 's':
